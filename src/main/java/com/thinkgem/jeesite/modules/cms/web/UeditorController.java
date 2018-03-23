93673601d231f84bf2351be5e99838ba4b12c8c3
cuipengyu <cuipy2004@163.com>
2018-03-23 12:28:43 +0800

    优化ueditor

--- src/main/java/com/thinkgem/jeesite/modules/cms/web/UeditorController.java
+++ src/main/java/com/thinkgem/jeesite/modules/cms/web/UeditorController.java
@@ -6,10 +6,13 @@ package com.thinkgem.jeesite.modules.cms.web;
 import com.baidu.ueditor.ActionEnter;
 import com.thinkgem.jeesite.common.config.Global;
 import com.thinkgem.jeesite.common.web.BaseController;
+import com.thinkgem.jeesite.modules.cms.ueditor.CuiActionEnter;
 import com.thinkgem.jeesite.modules.sys.utils.UserUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,12 +22,11 @@ import javax.servlet.http.HttpServletRequest;
  * @version 2018-3-22
  */
 @Controller
-@RequestMapping(value = "${adminPath}/ueditor/")
 public class UeditorController extends BaseController {
 
-
-
-	@RequestMapping(value = "/upload")
+	@RequiresPermissions("user")
+	@RequestMapping(value = "/ueditor/upload")
+	@ResponseBody
 	public String upload(HttpServletRequest req) {
 
 		String id=UserUtils.getPrincipal().getId();
@@ -32,7 +34,7 @@ public class UeditorController extends BaseController {
 		String rootPath = Global.getUserfilesBaseDir()+"userfiles/"+id+"/";
 		String jsonConfigPath = Global.getProjectRealPath()+"static/ueditor/config.json";
 
-        ActionEnter ae = new ActionEnter(req, rootPath);
+        CuiActionEnter ae = new CuiActionEnter(req, rootPath,jsonConfigPath);
 
         String res= ae.exec();
 
